754b5f5e113feea30a1e7f7908cc82abf07a2385
Srivathsan P <156175839+SrivathsanP23@users.noreply.github.com>
2024-08-22 10:38:37 +0530

    Add files via upload

--- file.py
+++ file.py
@@ -9,7 +9,7 @@ tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
 
 
 tab1.subheader("A tab with a chart")
-tab1.line_chart(df,x='State',y='seats_won')
+tab1.bar_chart(df,x='State',y='seats_won')
 
 tab2.subheader("A tab with the data")
 tab2.write(df)
